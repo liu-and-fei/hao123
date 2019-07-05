164b8e419c84e7dea9ec7ce3df94ccf0bba5d2c2
liufei <1272274017@qq.com>
2019-07-05 10:36:11 +0800

    123

--- src/store/index.js
+++ src/store/index.js
@@ -6,5 +6,5 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default createStore(
   combineReducers(reducers),
   composeEnhancers(applyMiddleware(thunk))
-  )
+)
 
