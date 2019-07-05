33c9c36360bc384961980bf9c054263b7b4cf629
huachao <1169257384@qq.com>
2019-07-05 11:04:51 +0800

    7个新闻板块

---
+++ src/views/class/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+import './index.css'
+
+import Tuijian from './tuijian/index';
+import Junshi from './junshi/index';
+import Yule from './yule/index';
+import Guoji from './guoji/index';
+import Shehui from './shehui/index';
+import Gaoxiao from './gaoxiao/index';
+import Tiyu from './tiyu/index';
+
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showdisplay: 'none'
+    }
+  }
+  render() {
+    return (
+      <div className="main-feed">
+        <div className="feed-fix" style={{ display: this.state.showdisplay }} >
+            <nav className="feed-nav">
+                <span><NavLink to={`/tuijian`}>推荐</NavLink></span>
+                <span><NavLink to={`/junshi`}>军事</NavLink></span>
+                <span><NavLink to={`/yule`}>娱乐</NavLink></span>
+                <span><NavLink to={`/guoji`}>国际</NavLink></span>
+                <span><NavLink to={`/shehui`}>社会</NavLink></span>
+                <span><NavLink to={`/gaoxiao`}>搞笑</NavLink></span>
+                <span><NavLink to={`/tiyu`}>体育</NavLink></span>
+            </nav>
+        </div>
+        <Switch>
+          <Route path={`/tuijian`} component={Tuijian} />
+          <Route path={`/junshi`} component={Junshi} />
+          <Route path={`/yule`} component={Yule} />
+          <Route path={`/guoji`} component={Guoji} />
+          <Route path={`/shehui`} component={Shehui} />
+          <Route path={`/gaoxiao`} component={Gaoxiao} />
+          <Route path={`/tiyu`} component={Tiyu} />
+          <Redirect to="/tuijian" />
+        </Switch>
+
+      </div>
+    );
+  }
+
+}
+
